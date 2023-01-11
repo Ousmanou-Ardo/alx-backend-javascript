d7c6b94b784e18b3fdca91105bc4f3baac2008ac
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2023-01-11 05:31:22 -0500

    var

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var tasking = true;
+    var task3 = false;
+  }
+
+  return [task, task2];
+}
